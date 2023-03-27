113d433f8e4272b464f407ce61b7422bf32d55f8
Soungjae <soungj94@gmail.com>
2023-03-26 23:54:01 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+// index.js
+//     findMatching()
+//       1) returns all drivers that match the passed in name
+//       2) returns matching drivers if case does not match but letters do
+//       3) returns an empty array if there is no match
+
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toUpperCase() === name.toUpperCase()
+  );
+}
+//     fuzzyMatch()
+//       4) returns a driver if beginning provided letters match
+//       5) does not return drivers if only middle or ending letters match
+//       6) does not return drivers if only middle or ending letters match
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(
+    (driver) =>
+      driver.slice(0, name.length).toLowerCase() === name.toLowerCase()
+  );
+}
+//     matchName()
+//       7) accesses the data structure to check if name matches
+function matchName(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.name.toLowerCase() === name.toLowerCase()
+  );
+}
